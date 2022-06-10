458c7f7062b57fc54729d56e24015b4b73013b90
Jeremy Gresham <jsg57@duke.edu>
2022-06-10 12:56:48 -0400

    updated

--- lib/Target/X86/X86TASENaiveChecks.cpp
+++ lib/Target/X86/X86TASENaiveChecks.cpp
@@ -386,7 +386,7 @@ void X86TASENaiveChecksPass::PoisonCheckPush(){
   //to have their original pre-clobbered values!)
   //Jnz as per sb_reopen in springboard.S to sb_eject
   //Example of adding symbol is in our addCartridgeSpringboard pass.
-  InsertInstr(X86::JLE_1)
+  InsertInstr(X86::JNE_1)
     .addExternalSymbol("sb_eject");
   std::cout << CurrentMI << std::endl;
   //Naive: Restore flags and rax here
@@ -550,7 +550,7 @@ void X86TASENaiveChecksPass::PoisonCheckMem(size_t size) {
   //to have their original pre-clobbered values!)
   //Jnz as per sb_reopen in springboard.S to sb_eject
   //Example of adding symbol is in our addCartridgeSpringboard pass.
-  InsertInstr(X86::JLE_1)
+  InsertInstr(X86::JNE_1)
     .addExternalSymbol("sb_eject");
   std::cout << CurrentMI << std::endl;
 
